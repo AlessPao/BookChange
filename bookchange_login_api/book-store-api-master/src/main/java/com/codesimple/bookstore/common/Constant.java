72e5bef176118631dc1823b19878be92e0125872
PAOLO RODRIGUEZ HERRERA <95638649+AlessPao@users.noreply.github.com>
2023-10-14 12:37:18 -0500

    Delete bookchange_login_api/book-store-api-master directory

--- bookchange_login_api/book-store-api-master/src/main/java/com/codesimple/bookstore/common/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.codesimple.bookstore.common;
-
-public interface Constant {
-
-    interface USER_TYPE{
-        String NORMAL = "NORMAL";
-        String ADMIN = "ADMIN";
-    }
-}
--- bookchange_login_api/book-store-api-master/src/main/java/com/codesimple/bookstore/repo/BookEditionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codesimple.bookstore.repo;
-
-import com.codesimple.bookstore.entity.BookEdition;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookEditionRepository extends CrudRepository<BookEdition, Long> {
-}
