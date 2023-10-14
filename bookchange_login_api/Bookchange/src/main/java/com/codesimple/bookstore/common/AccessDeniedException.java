24ed173217d64df4e216729395f39e7bf28b8adc
PAOLO RODRIGUEZ HERRERA <95638649+AlessPao@users.noreply.github.com>
2023-10-14 12:37:29 -0500

    Delete bookchange_login_api directory

--- bookchange_login_api/Bookchange/src/main/java/com/codesimple/bookstore/common/AccessDeniedException.java
+++
@@ -1,9 +0,0 @@
-package com.codesimple.bookstore.common;
-
-public class AccessDeniedException extends RuntimeException{
-    
-    public AccessDeniedException(String message){
-        super(message);
-    }
-    
-}
